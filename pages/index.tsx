7a1aa8a0717850b114ea2a4beac2238cf3669044
John Truong <trucojo@gmail.com>
2022-12-27 17:37:59 -0500

    [REF] Cleanup, Used Link component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,7 @@
-import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
-import styles from '../styles/Home.module.css';
 import Head from 'next/head';
-import { data, Song, Data } from '../data';
-import Card from '../components/Card';
-import dynamic from 'next/dynamic';
-const Animator = dynamic(
-    import('react-scroll-motion').then((it) => it.Animator),
-    { ssr: false }
-);
+import Link from 'next/link';
 
-import {
-    ScrollContainer,
-    ScrollPage,
-    batch,
-    Fade,
-    FadeIn,
-    FadeOut,
-    Move,
-    MoveIn,
-    MoveOut,
-    Sticky,
-    StickyIn,
-    StickyOut,
-    Zoom,
-    ZoomIn,
-    ZoomOut,
-} from 'react-scroll-motion';
-import { randomInt } from 'crypto';
-
-export const getStaticProps: GetStaticProps<{ songs: Song[] }> = async () => {
-    const top10 = data.top10.slice(0, 10).map((el: string) => data.entries[el]);
-    return {
-        props: {
-            songs: top10,
-        },
-    };
-};
-
-const Home = ({ songs }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Home = () => {
     return (
         <div>
             <Head>
@@ -54,8 +18,8 @@ const Home = ({ songs }: InferGetStaticPropsType<typeof getStaticProps>) => {
                 height: '100vh',
                 width: '100vw',
             }}>
-                <div><a href="/top10">Top 10</a></div>
-                <div><a href="/honorable_mentions">Honorable Mentions</a></div>
+                <Link href="/top10"><a>Top 10</a></Link>
+                <Link href="/honorable_mentions"><a>Honorable Mentions</a></Link>
             </div>
         </div>
     );
